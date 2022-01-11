68bcc75835871449699832b49d15173798f59ef3
Matt Hansen <mahans@microsoft.com>
2022-01-11 08:42:27 -0500

    linux dev

--- scripts/configureDNS.sh
+++ scripts/configureDNS.sh
@@ -1,13 +1,13 @@
 # Update & Install Prerequisites 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install bind9
+sudo apt install bind9 -y
 
 # Replace BIND config file
 wget https://raw.githubusercontent.com/matthansen0/azure-dnslb/dev/scripts/named.conf.options
 sudo cp /etc/bind/named.conf.options /etc/bind/named.conf.options.old
 sudo rm /etc/bind/named.conf.options
-sudo cp named.conf.options /etc/bind/named.conf.option
+sudo cp named.conf.options /etc/bind/named.conf.options
 
 #Validate and restart services
 sudo named-checkconf  /etc/bind/named.conf
