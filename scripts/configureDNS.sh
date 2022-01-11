1ad5c46cf643994132a1a07f3a79040df13dcb24
Matt Hansen <mahans@microsoft.com>
2022-01-11 12:30:41 -0500

    update branch

--- scripts/configureDNS.sh
+++ scripts/configureDNS.sh
@@ -4,7 +4,7 @@ sudo apt upgrade -y
 sudo apt install bind9 -y
 
 # Update BIND config file
-sudo curl https://raw.githubusercontent.com/matthansen0/azure-dnslb/dev/scripts/named.conf.options > /etc/bind/named.conf.options
+sudo curl https://raw.githubusercontent.com/matthansen0/azure-dnslb/main/scripts/named.conf.options > /etc/bind/named.conf.options
 
 #Validate and restart services
 sudo named-checkconf  /etc/bind/named.conf
